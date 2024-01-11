97028f02968ea8428d0e10f51c06bf6c928cf936
farneses <latriasouls@protonmail.com>
2024-01-11 15:02:15 -0300

    first commit

--- dart-docs/helloworld.dart
+++ dart-docs/helloworld.dart
@@ -1,3 +1,6 @@
 void main(){
-  print("Hello World");
+
+  var name = 'Farnese';
+  
+  print("My name is $name");
 }
---
+++ dart-docs/vars.dart
@@ -0,0 +1,11 @@
+void main(){
+  int number = 10;
+  int number1 = 20; 
+
+  print(number + number1);
+
+  String name = "Farnese";
+  int year = 18;
+
+  print("Hello $name, wow! you have $year old");
+}
