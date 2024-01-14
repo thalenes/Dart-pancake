16c72ef5b7ad44354d14c1b8a33f5e7abe8b1fc5
farneses <latriasouls@protonmail.com>
2024-01-14 19:46:43 -0300

    strings

--- OpenBootCamp/module2.5/bin/module2.dart
+++ OpenBootCamp/module2.5/bin/module2.dart
@@ -1,5 +1,29 @@
+import 'dart:mirrors';
+
 import 'package:module2/module2.dart' as module2;
 
 void main(List<String> arguments) {
-  print('Hello world: ${module2.calculate()}!');
+  String one = 'one';
+  String two = "two";
+
+  int four = 4;
+  String fourString = four.toString();
+
+  //print(four.runtimeType);
+  //print(fourString.runtimeType);
+  // print(fourString);
+
+  double peso = 72.5;
+  String pesoString = peso.toStringAsFixed(2);
+  print(pesoString);
+
+  String refran = "windows is a shit";
+  print(refran.contains("windows"));
+
+  String email = "contacto@gmail.com";
+  print(email.split("@")[1]);
+
+  print(refran.startsWith("MacOS"));
+
+  print(email.indexOf("@"));
 }
