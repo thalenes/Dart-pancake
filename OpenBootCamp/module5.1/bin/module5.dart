48fff0547e7272c7fca0b0f0d8ddcb5e39230628
farneses <latriasouls@protonmail.com>
2024-01-15 15:46:02 -0300

    if else

---
+++ OpenBootCamp/module5.1/bin/module5.dart
@@ -0,0 +1,11 @@
+import 'package:module5/module5.dart' as module5;
+
+void main(List<String> arguments) {
+  String sky = 'blue';
+
+  if (sky == 'blue') {
+    print('is day');
+  } else if (sky != 'blue') {
+    print('is night');
+  }
+}
---
+++ OpenBootCamp/module5.5/bin/module5.dart
@@ -0,0 +1,5 @@
+import 'package:module5/module5.dart' as module5;
+
+void main(List<String> arguments) {
+  print('Hello world: ${module5.calculate()}!');
+}
