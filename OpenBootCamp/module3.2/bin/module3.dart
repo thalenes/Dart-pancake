1bc55c20d66e1b2776f1ee800dd27dd98e4053de
farneses <latriasouls@protonmail.com>
2024-01-15 01:56:16 -0300

    added modules

---
+++ OpenBootCamp/module3.2/bin/module3.dart
@@ -0,0 +1,7 @@
+import 'package:module3/module3.dart' as module3;
+
+void main(List<String> arguments) {
+  var list = ["Farnese", 18, 1.71, "Bassonist"];
+  var lisInList = ["My List", list, 4];
+  print(lisInList);
+}
