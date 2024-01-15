291f3f6c6cea91868879448b376a2745c718237d
farneses <latriasouls@protonmail.com>
2024-01-15 02:29:41 -0300

    sets

--- OpenBootCamp/module3.4/bin/module3.dart
+++ OpenBootCamp/module3.4/bin/module3.dart
@@ -1,5 +1,14 @@
 import 'package:module3/module3.dart' as module3;
 
 void main(List<String> arguments) {
-  print('Hello world: ${module3.calculate()}!');
+  var firstSet = <String>{'Farnese', 'Ken', 'Axel'};
+
+  firstSet.add('Oldtraxx');
+  print(firstSet);
+
+  firstSet.remove('Ken');
+  print(firstSet);
+
+  firstSet.clear();
+  print(firstSet);
 }
